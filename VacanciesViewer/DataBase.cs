a80b6c8d60aeae2ca0e447708b5c859ed615a2c1
RomanovArtem <temik_romanov@mail.ru>
2017-06-07 14:50:19 +0300

    Добавьте файлы проекта.

---
+++ VacanciesViewer/DataBase.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Net.Mime;
+
+
+namespace VacanciesViewer
+{
+    class DataBase
+    {
+        /*    private readonly SqlConnection _sqlConnection;
+
+            public DataBase()
+            {
+                try
+                {
+                    var locationDb = MediaTypeNames.Application.StartupPath + @"\Database.mdf";
+                    var connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + locationDb;
+                    _sqlConnection = new SqlConnection(connectionString);
+                    _sqlConnection.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            public string GetVacancyTitle()
+            {
+                SqlDataReader sqlDataReader = null;
+                var command = new SqlCommand("SELECT * FROM Vacancy", _sqlConnection);
+                var html = "";
+                try
+                {
+                    sqlDataReader = command.ExecuteReader();
+                    var count = 0;
+                    while (sqlDataReader.Read())
+                    {
+                        count++;
+                        html += count + " " + Convert.ToString(sqlDataReader["title"]) + " " + Convert.ToString(sqlDataReader["url"]) + "\r\n";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    if (sqlDataReader != null)
+                        sqlDataReader.Close();
+                }
+                return html;
+            }
+
+            public void WriteDataDb(string title, string salary, string employer, string url, string requirement,
+                string responsibility, string address)
+            {
+                try
+                {
+                    using (SqlCommand command =
+                        new SqlCommand(
+                            "INSERT INTO Vacancy Values(@title, @salary, @employer, @url, @requirement, @responsibility, @address)",
+                            _sqlConnection))
+                    {
+                        command.Parameters.AddWithValue("title", title);
+                        command.Parameters.AddWithValue("salary", salary);
+                        command.Parameters.AddWithValue("employer", employer);
+                        command.Parameters.AddWithValue("url", url);
+                        command.Parameters.AddWithValue("requirement", requirement);
+                        command.Parameters.AddWithValue("responsibility", responsibility);
+                        command.Parameters.AddWithValue("address", address);
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+
+            public void DeleteDataDb()
+            {
+                var command = new SqlCommand("DELETE FROM Vacancy", _sqlConnection);
+                command.ExecuteNonQuery();
+            }*/
+    }
+}
---
+++ VacanciesViewer/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VacanciesViewer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlConnection cn;
+        private SqlDataAdapter da;
+        private DataSet ds;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+     
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            cn = new SqlConnection(@"Data Source=ARTEM\SQLEXPRESS;Initial Catalog=DataBaseVacancy;Integrated Security=true");
+
+            da = new SqlDataAdapter("Select * from Vacancy", cn);
+            ds = new DataSet();
+            da.Fill(ds);
+            dataGrid.ItemsSource = ds.Tables[0].DefaultView;
+        }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+      /*  public void DataSet Page_Load(int id)
+        {
+            var a = new DatabaseEntities();
+            var da = new SqlDataAdapter("SELECT * FROM Vacancy", );
+            return a.Vacancies;
+        }*/
+    }
+}
