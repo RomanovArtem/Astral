cd7b0b31c0f8d706e3e02e3aff723c4a6760ba39
RomanovArtem <temik_romanov@mail.ru>
2017-06-08 15:05:10 +0300

    Включил Newtonsoft.Json.dll в проект exe

--- VacanciesViewer/App.xaml.cs
+++ VacanciesViewer/App.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,18 @@ namespace VacanciesViewer
     /// </summary>
     public partial class App : Application
     {
+        public App() : base()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+        }
+
+        System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.Contains("Newtonsoft.Json"))
+            {
+                return Assembly.Load(VacanciesViewer.Properties.Resources.Newtonsoft_Json);
+            }
+            return null;
+        }
     }
 }
